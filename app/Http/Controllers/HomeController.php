1d205b7f31402c3ce2026177661ddad960fab9af
JF-GIAMMARI <giammari.jf@gmail.com>
2020-12-16 20:39:19 +0100

    Add Vue 1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Inertia\Inertia;
 class HomeController extends Controller
 {
     /**
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('welcome');
+        return Inertia::render("app/views/index.vue");
     }
 
     public function create()
