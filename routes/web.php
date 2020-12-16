3d1427ef0b916613c4a5ae44ffba2216591e8d0c
Baptiste <baptiste.varamo@gmail.com>
2020-12-16 18:24:00 +0100

    Route init

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return Inertia\Inertia::render('Dashboard');
+    return \Inertia\Inertia::render('Dashboard');
 })->name('dashboard');
