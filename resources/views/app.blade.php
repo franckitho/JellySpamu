a24d645f19ad3f601940287b12f97d23881b9ce8
JF-GIAMMARI <giammari.jf@gmail.com>
2020-12-17 10:32:16 +0100

    App Blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,8 +6,9 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/fontawesome.min.css">
-        
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+
+        <title>{{ config('app.name', 'Jellyspamu Converter') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -17,8 +18,28 @@
 
         <!-- Scripts -->
         @routes
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
         <script src="{{ mix('js/app.js') }}" defer></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.0/axios.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+
+        <script>
+        
+        function iconFormat(icon) {
+            var originalOption = icon.element;
+            if (!icon.id) { return icon.text; }
+            var $icon = '<i class="icon-' + $(icon.element).data('icon') + '"></i>' + icon.text;
+            return $icon;
+        }
+
+        // Initialize with options
+        $('.select-icons').select2({
+            templateResult: iconFormat,
+            minimumResultsForSearch: Infinity,
+            templateSelection: iconFormat,
+            escapeMarkup: function(m) { return m; }
+        });
+        </script>
     </head>
     <body class="font-sans antialiased">
         @inertia
